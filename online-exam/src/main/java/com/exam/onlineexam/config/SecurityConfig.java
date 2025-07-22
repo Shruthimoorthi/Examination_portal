fe6f93ff89a1279e7e85323f47b48fca7657790b
Shruthimoorthi <144005598+Shruthimoorthi@users.noreply.github.com>
2025-07-22 15:23:50 +0530

    Update SecurityConfig.java
    Update SecurityConfignew.java

--- online-exam/src/main/java/com/exam/onlineexam/config/SecurityConfig.java
+++ online-exam/src/main/java/com/exam/onlineexam/config/SecurityConfig.java
@@ -24,10 +24,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of(
-            "https://examination-portal-sable.vercel.app", 
-            "https://examination-portal-nkknt34wk-shruthimoorthis-projects.vercel.app"
-        ));
+        config.addAllowedOriginPattern("*");
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
